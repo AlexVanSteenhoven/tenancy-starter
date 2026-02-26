124d8c3894568e139990c1c162fa82f3866c7b18
Alex van Steenhoven <alex@modus-digital.com>
2026-02-26 11:18:06 +0100

    refactor(onboarding): Simplify onboarding actions by removing email notification and adding onboarding_email field

--- app/Actions/Onboarding/StoreSetupAccountAction.php
+++ app/Actions/Onboarding/StoreSetupAccountAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Actions\Onboarding;
 
+use App\Enums\Role;
 use App\Http\Requests\Onboarding\StoreSetupAccountRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
@@ -17,11 +18,15 @@ final readonly class StoreSetupAccountAction
     public function handle(StoreSetupAccountRequest $request): void
     {
         $user = DB::transaction(function () use ($request): User {
-            return User::create([
+            $_user = User::create([
                 'name' => $request->input('name'),
                 'email' => $request->input('email'),
                 'password' => $request->input('password'),
             ]);
+
+            $_user->assignRole(Role::Owner);
+
+            return $_user;
         });
 
         $user->sendEmailVerificationNotification();
